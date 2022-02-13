24ce6334593c442aaf2d630c0da8b2c1e478cbd1
BuildTools <unconfigured@null.spigotmc.org>
2022-02-13 12:10:10 -0700

    Current 2-13-22_12:09

---
+++ CKK.Logic/Models/StoreItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class StoreItem
+    {
+        private Product _product;
+        private int _quantity;
+
+        public StoreItem(Product product, int quantity)
+        {
+            _product = product;
+            _quantity = quantity;
+
+        }
+        
+        public int GetQuantity()
+        {
+            return _quantity; 
+
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            return _quantity;
+        }
+        public  Product GetProduct()
+        {
+            return _quantity;
+        }
+
+        public void SetProduct(Product _product)
+        {
+            return Product;
+        }
+
+       
+    }
+}
