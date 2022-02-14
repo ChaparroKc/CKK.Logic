ab0a448371a63540022099e7f5ce06338473cdfa
BuildTools <unconfigured@null.spigotmc.org>
2022-02-14 16:53:15 -0700

    Test 7

--- CKK.Logic/Models/Product.cs
+++ CKK.Logic/Models/Product.cs
@@ -12,37 +12,40 @@ namespace CKK.Logic.Models
         private string _name;
         private decimal _price;
         
+        int id;
+        string name;
+        decimal price;
         
 
         public int GetId()
         {
-            return Id;
+            return id;
         }
 
-        public void SetId(int Id)
+        public void SetId(int id)
         {
-            _id = Id;
+            _id = id;
         }
 
         public string GetName()
         {
-            return Name;
+            return name;
         }
 
-        public void SetName(string _name)
+        public void SetName(string name)
         {
-            _name = Name;
+            _name = name;
         }
 
         public decimal GetPrice()
         {
-            return Price;
+            return price;
 
         }
 
-        public void SetPrice(decimal _price)
+        public void SetPrice(decimal price)
         {
-            _price = Price;
+            _price = price;
         }
     }
 
