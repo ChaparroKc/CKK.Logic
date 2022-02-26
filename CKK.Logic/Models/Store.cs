5e3ee71a1e97d8b82e19b52e43684af1b150006a
Kennan Chaparro <chaparroke@outlook.com>
2022-02-26 15:25:39 -0700

    Test-5

--- CKK.Logic/Models/Store.cs
+++ CKK.Logic/Models/Store.cs
@@ -40,26 +40,23 @@ namespace CKK.Logic.Models
         {
             if(prod == Product1)
             {
-                Product1 = new Product(prod);
+                Product1 = new Product(Name);
                 return Product1;
             }
             else if(prod == Product2)
             {
-               Product2 = new Product(prod);
+               Product2 = new Product(Name);
                 return Product2;
             }
             else if(prod == Product3)
             {
-                Product3 = new Product(prod);
+                Product3 = new Product(Name);
                 return Product3;
             }
             else
             {
                 return null;
             }
-            return prod;
-
-
         }
 
         public Product RemoveStoreItem(int productNum)
