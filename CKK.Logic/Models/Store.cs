d1f3a82317ddea35b4f21de04594b089ad9edfb0
Kennan Chaparro <chaparroke@outlook.com>
2022-03-02 18:54:52 -0700

    .

--- CKK.Logic/Models/Store.cs
+++ CKK.Logic/Models/Store.cs
@@ -39,18 +39,15 @@ namespace CKK.Logic.Models
             if(_product1 == null)
             {
                 _product1 = product;
-                return _product1;
+                
             }else if(_product2 == null)
             {
                _product2 = product;
-               return _product2;
+               
             }else if(_product3 == null)
             {
                 _product3 = product;
-                return _product3;
-            }else
-            {
-                return null;
+                
             }
         }
 
