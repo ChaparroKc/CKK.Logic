75f2925001a8832c2b9bb9b7e57a72041882c1bb
Kennan Chaparro <chaparroke@outlook.com>
2022-03-23 17:20:36 -0600

    current

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -42,8 +42,85 @@ namespace CKK.Logic.Models
 
 		public ShoppingCartItem AddProduct(Product prod)
         {
+			if (prod == 1)
+            {
+				_product1 = prod;
+					
+            }
+			if (prod == 2)
+            {
+				_product2 = prod;
+            }
+			if (prod == 3)
+            {
+				_product3 = prod;
+            }
+			else
+            {
+				return null;
+            }
 
         }
+
+		public ShoppingCartItem RemoveProduct(Product prod, int quantity)
+        {
+			if (prod == 1)
+            {
+				_product1 -= quantity;
+
+            }
+			if (prod == 2)
+            {
+				_product2 -= quantity;
+            }
+			if (prod == 3)
+            {
+				_product3 -= quantity;
+            }
+            else
+            {
+				return null;
+            } 
+        }
+
+		public ShoppingCartItem GetProductById(int id)
+        {
+			if ( id == 1)
+            {
+				return _product1;
+            }
+			if ( id == 2)
+            {
+				return _product2;
+            }
+			if ( id == 3)
+            {
+				return _product3;
+			}
+        }
+
+		public decimal GetTotal()
+        {
+			return quantity * Product.GetPrice();
+        }
+
+		public ShoppingCartItem GetProduct(int productNum)
+        {
+			if (productNum == 1)
+            {
+				return _product1;
+            }
+			if (productNum == 2)
+            {
+				return _product2;
+            }
+			if (productNum == 3)
+            {
+				return _product3;
+            }
+        }
+
+
 
 
 	}
