aa340d0b9883d800ae4ae940cff5fd2ba0cdcfb8
Kennan Chaparro <chaparroke@outlook.com>
2022-03-24 17:24:03 -0600

    .

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -23,16 +23,16 @@ namespace CKK.Logic.Models
         {
 			if (ShoppingCartItem == _product1)
             {
-				ShoppingCartItem(quantity) = quantity;
+				ShoppingCartItem(quantity) += quantity;
 
             }
 			if (ShoppingCartItem == _product2)
             {
-				ShoppingCartItem(quantity) = quantity;
+				ShoppingCartItem(quantity) += quantity;
             }
 			if (ShoppingCartItem == _product3)
             {
-				ShoppingCartItem(quantity) = quantity;
+				ShoppingCartItem(quantity) += quantity;
             }
             else
             {
@@ -66,16 +66,16 @@ namespace CKK.Logic.Models
         {
 			if (prod == 1)
             {
-				_product1 = quantity;
+				ShoppingCartItem(quantity) -= quantity;
 
             }
 			if (prod == 2)
             {
-				_product2 = quantity;
+				ShoppingCartItem(quantity) -= quantity;
             }
 			if (prod == 3)
             {
-				_product3 = quantity;
+				ShoppingCartItem(quantity) -= quantity;
             }
             else
             {
