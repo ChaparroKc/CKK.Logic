21d35237ab2fb2d615886acbc8094164c99698db
Kennan Chaparro <chaparroke@outlook.com>
2022-05-11 17:48:41 -0600

    .

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -19,21 +19,21 @@ namespace CKK.Logic.Models
 			return _customer;
         }
 
-		public ShoppingCartItem AddProduct(Product prod, int _quantity)
+		public ShoppingCartItem.AddProduct(Product prod, int _quantity)
         {
 			if( _quantity < 1)
             {
 				return null;
             }
-			if(_product1 != null && product.GetProduct().GetId == prod.GetId())
+			if(_product1 != null && _product.GetProduct().GetId == prod.GetId())
             {
 				ShoppingCartItem.SetQuantity() += _quantity;
             }
-			if(_product2 != null && product.GetProduct().GetId == prod.GetId())
+			if(_product2 != null && _product.GetProduct().GetId == prod.GetId())
             {
 				ShoppingCartItem.SetQuantity() += _quantity;
             }
-			if(_product3 != null && product.GetProduct().GetId == prod.GetId())
+			if(_product3 != null && _product.GetProduct().GetId == prod.GetId())
             {
 				ShoppingCartItem.SetQuantity() += _quantity;
             }
