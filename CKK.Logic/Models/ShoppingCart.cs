8ba9ebf2fbf5ac56febf0331035ee8835daae7be
Kennan Chaparro <chaparroke@outlook.com>
2022-05-11 18:47:38 -0600

    .

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -30,6 +30,10 @@ namespace CKK.Logic.Models
 				ShoppingCartItem _product1 = new ShoppingCartItem(prod, quantity);
 				
 			}
+            else
+            {
+				return ShoppingCartItem.SetQuantity()++;
+            }
 			if(_product2 != null && _product2.GetProduct().GetId() == prod.GetId())
             {
 				ShoppingCartItem _product2 = new ShoppingCartItem(prod, quantity);
